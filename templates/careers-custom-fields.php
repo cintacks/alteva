0f3099e48d975fa979df5394aefe9760a7e5a2e4
root <root@rqs6-6c6t.accessdomain.com>
2013-12-04 00:43:02 -0500

    Added Apply Now link to Career display.

--- templates/careers-custom-fields.php
+++ templates/careers-custom-fields.php
@@ -1,9 +1,16 @@
 <?php
 $pod = get_current_pod();
 
-$location         = trim($pod->field('location'));
-$requirements     = trim($pod->field('requirements'));
-$responsibilities = trim($pod->field('responsibilities'));
+$fields = array(
+  'location',
+  'requirements',
+  'responsibilities',
+  'apply_url'
+);
+
+foreach($fields as $field_name) {
+  $$field_name = trim($pod->field($field_name));
+}
 
 if(!empty($location)) {
   echo "<p><strong>Location</strong>: {$location}</p>";
@@ -15,4 +22,9 @@ if(!empty($requirements)) {
 
 if(!empty($responsibilities)) {
   echo "<p><strong>Responsibilities:</strong><br />{$responsibilities}</p>";
+}
+
+if(!empty($apply_url)) {
+  $target = strpos($apply_url, "mailto") === 0 ? "" : " target='_blank'";
+  echo "<p><a href='{$apply_url}'{$target}>Apply Now</a></p>";
 }
