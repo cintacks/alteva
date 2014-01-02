8c9faee30b65276126ed48c115634876826c8efe
Tim Mannino <tim@kettul.com>
2014-01-02 12:40:27 -0500

    Updates to spacing, slider splicing, wide layout styles, and menu fixes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -208,21 +208,21 @@ $(document).ready(function() {
 });
 
     //Swipe support for the slideshow
-    $(document).ready(function() {  
+    // $(document).ready(function() {  
  
-          //Enable swiping...
-          $(".carousel-inner").swipe( {
-            //Generic swipe handler for all directions
-            swipeLeft:function(event, direction, distance, duration, fingerCount) {
-              $(this).parent().carousel('prev'); 
-            },
-            swipeRight: function() {
-              $(this).parent().carousel('next'); 
-            },
-            //Default is 75px, set to 0 for demo so any distance triggers swipe
-            threshold:0
-          });
-        });   
+    //       //Enable swiping...
+    //       $(".carousel-inner").swipe( {
+    //         //Generic swipe handler for all directions
+    //         swipeLeft:function(event, direction, distance, duration, fingerCount) {
+    //           $(this).parent().carousel('prev'); 
+    //         },
+    //         swipeRight: function() {
+    //           $(this).parent().carousel('next'); 
+    //         },
+    //         //Default is 75px, set to 0 for demo so any distance triggers swipe
+    //         threshold:0
+    //       });
+    //     });   
 
 
 
