4f4b1959773164e3313c88ffa2feff24c8908006
Tim Mannino <tim@kettul.com>
2014-01-06 13:55:42 -0500

    Attempt to fix Windows 8 phone display issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+//Fix Windows 8 Viewport
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement("style");
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      "@-ms-viewport{width:auto!important}"
+    )
+  );
+  document.getElementsByTagName("head")[0].
+    appendChild(msViewportStyle);
+}
+
+
 function flagDropdownsWithMultipleItems(parent) {
   parent.find('ul').each(function() {
     if($(this).children().length > 1) {
